6d92dd6f518b91928acd449322a60b8a04bef954
pro <pro@prodeMacBook-Pro.local>
2014-10-20 10:26:55 +0800

    Initial Commit

---
+++ 缓存例子/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  缓存例子
+//
+//  Created by pro on 14-10-20.
+//  Copyright (c) 2014年 jkrm. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
